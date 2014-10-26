d31fd9959420f74742d53cd1def15a5a00436726
Mack Ward <mackorone@gmail.com>
2014-10-26 14:33:24 -0400

    Added dialation

--- src/c2.py
+++ src/c2.py
@@ -1,31 +1,28 @@
 import cv2
 import numpy as np
 
+# Get the image and convert to HSV
 img = cv2.imread('../data/m7/IMG_0290.JPG',cv2.CV_LOAD_IMAGE_COLOR)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+# Specify the red range for the color mask
 lower_red = np.array([0,0,0])
 upper_red = np.array([10,255,255])
+red_mask = cv2.inRange(hsv, lower_red, upper_red)
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-mask = cv2.inRange(hsv, lower_red, upper_red)
-res = cv2.bitwise_and(hsv, hsv, mask = mask)
+res = cv2.bitwise_and(hsv, hsv, mask = red_mask)
 
-cop = res.copy()
-for row in range(len(res)):
-    for col in range(len(res[0])):
-        if all([res[row][col][i] != 0 for i in [1,2]]):
-            r = 5
-            for i in range(-r,r):
-                for j in range(-r,r):
-                    if (0 <= row + i and row + i < len(res)
-                    and 0 <= col + j and col + j < len(res[0])):
-                        cop[row+i][col+j][0] = 0
-                        cop[row+i][col+j][1] = 255
-                        cop[row+i][col+j][2] = 255
+#dialation_size = 
+dialation_kernal = np.ones((5,5),np.float32)/25
+mack = cv2.dilate(res,dialation_kernal,iterations = 1)
 
-rgb = cv2.cvtColor(cop, cv2.COLOR_HSV2BGR)
+res2 = cv2.bitwise_and(mack, mack, mask = red_mask)
+mack2 = cv2.dilate(res2,kern,iterations = 1)
 
-size = 5
+#mack = cv2.morphologyEx(res, cv2.MORPH_CLOSE, kern)
+rgb = cv2.cvtColor(mack2, cv2.COLOR_HSV2BGR)
+
+size = 1
 kernel = np.ones((size,size),np.float32)/size**2
 blurred = cv2.filter2D(rgb,-1,kernel)
 
