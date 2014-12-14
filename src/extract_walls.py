eba007400fc5534cecc04088ffeb084a59120de3
Mack Ward <mackorone@gmail.com>
2014-12-14 00:14:44 -0500

    Got the wall detection working

--- src/extract_walls.py
+++ src/extract_walls.py
@@ -27,6 +27,7 @@ cv2.waitKey()
 
 image_path = 'topdown_cropped.jpg'
 color_img = cv2.imread(image_path, cv2.CV_LOAD_IMAGE_COLOR)
+draw_img = color_img.copy()
 ave_lines = hough_lines(image_path)
 group1, group2 = get_groups_of_lines(rho_theta_to_x1y1_x2y2(ave_lines, np.shape(color_img)), np.shape(color_img))
 
@@ -53,5 +54,49 @@ rows = [points[5*i:5*i + 5] for i in range(7)]
 # Then sort by x-value
 rows = [sorted(r, key = lambda x: x[0]) for r in rows]
 
-# Now, we just need to check the pixels within pairs of points
+# Create the red mask for determining whether or not walls exist
+lower_red = np.array([0,10,155])
+upper_red = np.array([10,255,255])
 
+def horizontal_wall_exists(img, left, right):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    y = int((left[1] + right[1])/2)
+    window = hsv[y-25:y+25, left[0]+15:right[0]-15, :]
+    red_mask = cv2.inRange(window, lower_red, upper_red)
+    return sum(sum(red_mask)) > 500
+
+def vertical_wall_exists(img, top, bottom):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    x = int((top[0] + bottom[0])/2)
+    window = hsv[top[1]+15:bottom[1]-15, x-25:x+25, :]
+    red_mask = cv2.inRange(window, lower_red, upper_red)
+    return sum(sum(red_mask)) > 500
+
+# The tiles are numbered as follows:
+# |  0  1  2  3 |
+# |  4  5  6  7 |
+# |  8  9 10 11 |
+# | 12 13 14 15 |
+# | 16 17 18 19 |
+# | 20 21 22 23 |
+walls = [[0,0,0,0] for i in range(24)]
+
+# Check all pairs of points along the horizonal
+for i in range(len(rows)):
+    for j in range(len(rows[i])-1):
+        if horizontal_wall_exists(color_img, rows[i][j], rows[i][j+1]):
+            center = ((rows[i][j][0] + rows[i][j+1][0])/2, (rows[i][j][1] + rows[i][j+1][1])/2)
+            # TODO: Add to walls
+            # walls[
+            cv2.circle(draw_img, center, 10, (0, 0, 255), -1)
+
+# Check all pairs of points along the vertical
+for i in range(len(rows[0])):
+    for j in range(len(rows)-1):
+        if vertical_wall_exists(color_img, rows[j][i], rows[j+1][i]):
+            center = ((rows[j][i][0] + rows[j+1][i][0])/2, (rows[j][i][1] + rows[j+1][i][1])/2)
+            # TODO: Add to walls
+            cv2.circle(draw_img, center, 10, (0, 0, 255), -1)
+
+cv2.imshow('foo', draw_img)
+cv2.waitKey()
